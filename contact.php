09e7d4803ed1ea23cd099af4fa33b9dc4aeb2d56
949919 <99135921+949919@users.noreply.github.com>
2022-02-08 15:02:52 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,33 @@
+<?php
+
+if (empty($_POST['submit']))
+
+{ echo "Form is not submitted!";
+
+exit;
+
+}
+
+if (empty($_POST["fullname"] ) || empty($_POST["gender"] ) || empty($_POST["email"] )
+
+)
+{
+
+echo "Please Fill the Form";
+
+exit;
+
+}
+
+$name = $_POST["fullname"];
+
+$email = $_POST["email"];
+
+$gender = $_POST["gender"];
+
+mail ('yk861595@gmail.com','New Form
+
+Submission',"New Form Submission:Name:$name,Gender:
+$gender,Email:$email" );
+
+?>
